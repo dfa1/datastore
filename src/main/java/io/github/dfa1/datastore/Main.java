cf5bc9d61bf05dd0a3bb0cc7f131e334bb78535b
Davide Angelocola <davide.angelocola@gmail.com>
2026-05-24 09:11:04 +0200

    chore: drop Main and MainTest

--- src/main/java/io/github/dfa1/datastore/Main.java
+++
@@ -1,8 +0,0 @@
-package io.github.dfa1.datastore;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("datastore: CSV vs Parquet vs Vortex benchmark");
-    }
-}
